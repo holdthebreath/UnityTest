3fa5c9844a250af65bb108cfb052934ce23efbe2
holdthebreath <hwdxxy@gmail.com>
2019-02-17 10:01:15 +0800

    project initialization

---
+++ src/main/java/unity/test/controller/LogInController.java
@@ -0,0 +1,34 @@
+package unity.test.controller;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import unity.test.pojo.ReturnData;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class LogInController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "/Test/index";
+    }
+
+    @CrossOrigin
+    @RequestMapping("/login")
+    @ResponseBody
+    public String logIn() throws Exception{
+        HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest();
+        System.out.println(request.getParameter("UserName"));
+        ReturnData returnData = new ReturnData();
+        returnData.setData("wsnd");
+        Gson gson = new Gson();
+        return gson.toJson(returnData);
+    }
+}
---
+++ src/main/java/unity/test/pojo/ReturnData.java
@@ -0,0 +1,13 @@
+package unity.test.pojo;
+
+public class ReturnData {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
