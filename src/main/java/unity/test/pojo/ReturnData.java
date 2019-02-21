610cfd49a07c98244c59ec6c2ec12e2c78c6f657
holdthebreath <hwdxxy@gmail.com>
2019-02-21 16:10:31 +0800

    update pojo

--- src/main/java/unity/test/pojo/ReturnData.java
+++ src/main/java/unity/test/pojo/ReturnData.java
@@ -1,7 +1,41 @@
 package unity.test.pojo;
 
 public class ReturnData {
+    private float x, y, down, up;
     private String data;
+
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getDown() {
+        return down;
+    }
+
+    public void setDown(float down) {
+        this.down = down;
+    }
+
+    public float getUp() {
+        return up;
+    }
+
+    public void setUp(float up) {
+        this.up = up;
+    }
 
     public String getData() {
         return data;
